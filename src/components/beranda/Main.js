aa33f480765b011b459a4e06c431b6e2cc95b573
fajar <fapucipulo@gmail.com>
2021-05-11 09:57:58 +0700

    Revisi beranda

--- src/components/beranda/Main.js
+++ src/components/beranda/Main.js
@@ -14,8 +14,8 @@ export function Main() {
                 <div className="content">
                     <div className="content-left">
                         <h6>Selamat Datang di Camakara</h6>
-                        <h1>Penyelenggara Tryout UTBK Terpercaya di Indonesia</h1>
-                        <p>Camakara menjadi salah satu platform terpercaya di Indonesia dalam menyelenggarakan, menyediakan informasi seputar tryout UTBK agar kalian dapat semakin bersemangat dalam memperjuangkan perguruan tinggi yang diimpikannya.</p>
+                        <h1>Penyelenggara Tryout UTBK Tepercaya di Indonesia</h1>
+                        <p>Camakara menjadi salah satu platform tepercaya di Indonesia dalam menyelenggarakan, menyediakan informasi seputar tryout UTBK agar kalian dapat semakin bersemangat dalam memperjuangkan perguruan tinggi yang diimpikannya.</p>
 
                         <form>
                             <input type="text" placeholder="Tryout UTBK 2021" />
